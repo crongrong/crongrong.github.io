4f2269c24af93a20065e88a2fbcd4296a623aec9
crongrong <243608770@qq.com>
2016-11-14 14:49:15 +0800

    w10gai01

--- d3/js/myd3.js
+++ d3/js/myd3.js
@@ -4,18 +4,28 @@
      //<rect x="100" y="130" width="40" height="40" style="fill:none;stroke:black;stroke-width:3;opacity:0.5" />
   var svgctrl = d3.select('svg'); 
 
+  var cx = 50;
+
   var c1 = svgctrl.append('rect');
 
-  c1.attr('x', 50).attr('y', 20).attr('rx', 20).attr('ry', 20).attr('width', 150).attr('height', 150).attr('fill', 'red').attr('stroke', 'black').attr('stroke-width', 5).attr('opacity', 0.5);
+  c1.attr('x', cx).attr('y', cx - 30).attr('rx', 20).attr('ry', 20).attr('width', 150).attr('height', 150).attr('fill', 'red').attr('stroke', 'black').attr('stroke-width', 5).attr('opacity', 0.5);
 
   var c2 = svgctrl.append('line');
 
-   c2.attr('x1', 100).attr('y1', 60).attr('x2', 70).attr('y2', 80).attr('stroke', 'black').attr('stroke-width', 3);
+   c2.attr('x1', cx + 50).attr('y1', cx + 10).attr('x2', cx + 20).attr('y2', cx +30).attr('stroke', 'black').attr('stroke-width', 3);
 
    var c3 = svgctrl.append('line');
 
-   c3.attr('x1', 140).attr('y1', 60).attr('x2', 170).attr('y2', 80).attr('stroke', 'black').attr('stroke-width', 3);
+   c3.attr('x1', cx + 90).attr('y1', cx + 10).attr('x2', cx + 120).attr('y2', cx + 30).attr('stroke', 'black').attr('stroke-width', 3);
 
     var c4 = svgctrl.append('rect');
 
-  c4.attr('x', 100).attr('y', 130).attr('width', 40).attr('height', 40).attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', 3).attr('opacity', 0.5);
+  c4.attr('x', cx + 50).attr('y', cx + 80).attr('width', 40).attr('height', 40).attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', 3).attr('opacity', 0.5);
+
+  var btn_click = document.getElementById("move");
+  var box = document.querySelector(".col-xl-12");
+  var a = 0;
+  btn_click.onclick = function(){
+    a =(a+50)%500;
+    box.style.left =a+'px';
+  }
